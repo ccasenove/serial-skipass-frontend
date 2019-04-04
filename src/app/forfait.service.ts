3fb19f1d1b4434460bf61f34f8df8a2cae2ba50a
C4z <casenove.cedric@gmail.com>
2019-04-04 18:45:37 +0200

    Modification urls backend

--- src/app/forfait.service.ts
+++ src/app/forfait.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { of } from 'rxjs';
 
-const FORFAIT_ID = "123";
+const FORFAIT_ID = 200;
 
 const API_ROOT = '/rest/api/v1'
 
@@ -14,13 +14,11 @@ export class ForfaitService {
   constructor(private httpClient: HttpClient) { }
 
   public startConso(stationId: number) {
-    return this.httpClient.put(`${API_ROOT}/forfait/${FORFAIT_ID}/start`, {
-      stationId
-    });
+    return this.httpClient.put(`${API_ROOT}/forfaits/${FORFAIT_ID}/start`, stationId);
   }
 
   public stopConso() {
-    return this.httpClient.put(`${API_ROOT}/forfait/${FORFAIT_ID}/stop`, null);
+    return this.httpClient.put(`${API_ROOT}/forfaits/${FORFAIT_ID}/stop`, null);
   }
 
   public getStations() {
